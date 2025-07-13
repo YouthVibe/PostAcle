0e938ebd6301167159a7c7075da9defc9f93b97f
UltronTheAI <coderswaraj@gmail.com>
2025-07-13 15:03:49 +0530

    Developmental Fix

--- post-acle/next.config.ts
+++ post-acle/next.config.ts
@@ -4,14 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: "export", // Enable static export
+
   images: {
-    unoptimized: true, // Required for static export
+
     formats: ["image/webp"],
     deviceSizes: [320, 420, 768, 1024, 1200], // ✅ valid key
     imageSizes: [16, 32, 48, 64, 96],         // ✅ optional additional sizes
   },
-  trailingSlash: true,
+
 };
 
 export default nextConfig;
