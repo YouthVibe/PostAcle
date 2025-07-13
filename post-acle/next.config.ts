21c09b749d341cf7e80530e3faf712d3dd1df9bc
UltronTheAI <coderswaraj@gmail.com>
2025-07-13 21:57:37 +0530

    Readme.md

--- post-acle/next.config.ts
+++ post-acle/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-
+    remotePatterns: [new URL('https://www.post-acle.blog/images/**')],
     formats: ["image/webp"],
     deviceSizes: [320, 420, 768, 1024, 1200], // ✅ valid key
     imageSizes: [16, 32, 48, 64, 96],         // ✅ optional additional sizes
