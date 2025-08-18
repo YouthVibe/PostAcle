fc971a99cf24e7ee6a8e7dc20fc6487731e82570
UltronTheAI <coderswaraj@gmail.com>
2025-08-18 22:33:22 +0530

    Some Ui updates

--- post-acle/app/blog/[id]/BlogPostContent.tsx
+++ post-acle/app/blog/[id]/BlogPostContent.tsx
@@ -250,7 +250,7 @@ export default function BlogPostContent({ blogPost }: BlogPostContentProps) {
                 
                 // Match icons to tag content
                 const getIcon = (tag: string) => {
-                  const iconMap: {[key: string]: JSX.Element} = {
+                  const iconMap: {[key: string]: any} = {
                     'tech': <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>,
                     'programming': <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" /></svg>,
                     'web': <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" /></svg>,
