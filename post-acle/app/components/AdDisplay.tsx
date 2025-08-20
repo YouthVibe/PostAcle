1da508e38b230a56ea4d5479d260d4ea0af602dc
UltronTheAI <coderswaraj@gmail.com>
2025-08-20 19:58:51 +0530

    Setup Ads System

--- post-acle/app/components/AdDisplay.tsx
+++ post-acle/app/components/AdDisplay.tsx
@@ -36,7 +36,7 @@ export default function AdsDisplay({ adType }: { adType: string }) {
   useEffect(() => {
     const fetchAds = async () => {
       try {
-        const res = await fetch('/ads/index.json');
+        const res = await fetch('/Ads/index.json');
         const data = await res.json();
         setAds(data);
       } catch (error) {
